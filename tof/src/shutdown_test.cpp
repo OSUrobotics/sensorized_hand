6062bcb97aa94389cf7e31703064b08bfed1d442
dufrenekm <dufrenekm@gmail.com>
2023-12-04 03:08:24 +0000

    Updated to 4 targets per zone

--- tof/src/shutdown_test.cpp
+++ tof/src/shutdown_test.cpp
@@ -37,6 +37,7 @@ class MinimalPublisher : public rclcpp::Node
 		tof_publisher_ = this->create_publisher<hand_msgs::msg::Tof64>("tof_msg", 10);
 		timer_ = this->create_wall_timer(
 		500ms, std::bind(&MinimalPublisher::timer_callback, this));
+		
 		// Setup and start the sensor
 		tof_setup();
 		// tof_shutdown();
@@ -44,6 +45,7 @@ class MinimalPublisher : public rclcpp::Node
 		
 
 	}
+
   private:
   	VL53L7CX_Configuration 	Dev;
 	uint16_t left_sensor = 0x52;
@@ -59,7 +61,7 @@ class MinimalPublisher : public rclcpp::Node
 		status = sensor_bringup(&Dev, left_sensor);
 		if (status) {
 			// Something went wrong, throw an error and shutdown node
-			RCLCPP_ERROR(this->get_logger(), "My log message %d", 4);
+			RCLCPP_ERROR(this->get_logger(), "Sensor %d bringup failed.", left_sensor);
 			rclcpp::shutdown();
 		} 
 	}
@@ -78,7 +80,7 @@ class MinimalPublisher : public rclcpp::Node
 	void publish_tof()
 	{
 		int status;
-		uint8_t isReady, i, loop;
+		uint8_t isReady, i, j, loop;
 		VL53L7CX_ResultsData 	Results;
 		auto tof_all_zones = hand_msgs::msg::Tof64();
 
@@ -89,53 +91,57 @@ class MinimalPublisher : public rclcpp::Node
 
 			if(isReady)
 			{
-				printf("ready!!!!");
 				// This gets and returns the actual distance data
 				vl53l7cx_get_ranging_data(&Dev, &Results);
 
 				/* As the sensor is set in 4x4 mode by default, we have a total 
 				* of 16 zones to print. For this example, only the data of first zone are 
 				* print */
-				printf("Print data no : %3u\n", &Dev.streamcount);
+				// printf("Print data no : %3u\n", &Dev.streamcount);
 				for(i = 0; i < 16; i++)
 				{
-					printf("Zone: %3d, Status: %3u, Distance: %4d mm, Ambient per: %4d , NBtargetdetect: %4d, Signal: %8d\n",
-						i,
-						Results.target_status[VL53L7CX_NB_TARGET_PER_ZONE*i],
-						Results.distance_mm[VL53L7CX_NB_TARGET_PER_ZONE*i],
-						Results.ambient_per_spad[VL53L7CX_NB_TARGET_PER_ZONE*i],
-						Results.nb_target_detected[VL53L7CX_NB_TARGET_PER_ZONE*i],
-						Results.signal_per_spad[VL53L7CX_NB_TARGET_PER_ZONE*i]);
+					// printf("Zone: %3d, Status: %3u, Distance: %4d mm, Ambient per: %4d , NBtargetdetect: %4d, Signal: %8d\n",
+					// 	i,
+					// 	Results.target_status[VL53L7CX_NB_TARGET_PER_ZONE*i],
+					// 	Results.distance_mm[VL53L7CX_NB_TARGET_PER_ZONE*i],
+					// 	Results.ambient_per_spad[VL53L7CX_NB_TARGET_PER_ZONE*i],
+					// 	Results.nb_target_detected[VL53L7CX_NB_TARGET_PER_ZONE*i],
+					// 	Results.signal_per_spad[VL53L7CX_NB_TARsGET_PER_ZONE*i]);
 					//printf("%d", VL53L7CX_NB_TARGET_PER_ZONE);
 					auto tof_zone = hand_msgs::msg::Tofzone();
 					tof_zone.zone_num = i;
-					tof_zone.distance = Results.distance_mm[VL53L7CX_NB_TARGET_PER_ZONE*i];
-					tof_zone.status = Results.target_status[VL53L7CX_NB_TARGET_PER_ZONE*i];
-					tof_zone.sigma = Results.range_sigma_mm[VL53L7CX_NB_TARGET_PER_ZONE*i];
 					tof_zone.ambient = Results.ambient_per_spad[VL53L7CX_NB_TARGET_PER_ZONE*i];
 					tof_zone.num_target = Results.nb_target_detected[VL53L7CX_NB_TARGET_PER_ZONE*i];
 					tof_zone.num_spad = Results.nb_spads_enabled[VL53L7CX_NB_TARGET_PER_ZONE*i];
-					tof_zone.signal = Results.signal_per_spad[VL53L7CX_NB_TARGET_PER_ZONE*i];
-					tof_zone.reflectance = Results.reflectance[VL53L7CX_NB_TARGET_PER_ZONE*i];
+
+					for (j = 0; j < VL53L7CX_NB_TARGET_PER_ZONE; j++) {
+						uint16_t idx = VL53L7CX_NB_TARGET_PER_ZONE * i + j;
+						tof_zone.distance[j] = Results.distance_mm[idx];
+						tof_zone.status[j] = Results.target_status[idx];
+						tof_zone.sigma[j] = Results.range_sigma_mm[idx];
+						tof_zone.signal[j] = Results.signal_per_spad[idx];
+						tof_zone.reflectance[j] = Results.reflectance[idx];
+					}
 					tof_all_zones.tof_array[i] = tof_zone;
 				}
-
+				break;
 			}
-				printf("\n");
-				loop++;
-				WaitMs(&Dev.platform, 5);
-			}
+			printf("\n");
+			loop++;
+			WaitMs(&Dev.platform, 10);
+		}
 			tof_publisher_->publish(tof_all_zones);
-			printf("publish!!!!");
+			printf("\npublish!!!!\n");
+			
 	}
 	
     void timer_callback()
     {	
 		publish_tof();
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
+    //   auto message = std_msgs::msg::String();
+    //   message.data = "Hello, world! " + std::to_string(count_++);
+    //   RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    //   publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
@@ -151,19 +157,6 @@ class MinimalPublisher : public rclcpp::Node
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-
-	// status = vl53l7cx_stop_ranging(&Dev);
-	// printf("End of ULD demo\n");
-
-	// status = vl53l7cx_set_power_mode(&Dev, VL53L7CX_POWER_MODE_SLEEP);
-	// if(status)
-	// {
-	// 	printf("VL53L7CX sleep failed\n");
-	// 	return status;
-	// }
-
-	// printf("Starting examples with ULD version %s\n", VL53L7CX_API_REVISION);
-//   VL53L7CX_Configuration 	Dev_1;
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
   return 0;
